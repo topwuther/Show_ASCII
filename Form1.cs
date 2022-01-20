7ea405a8294c4ee9f82845f3cc20e48794786936
topwuther <topwuther@gmail.com>
2022-01-21 02:16:21 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Show_ASCII
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string temp = "";
+            foreach (var item in textBox1.Text)
+            {
+                temp += (int)item + " ";
+            }
+            label1.Text = temp;
+        }
+    }
+}
